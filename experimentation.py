f6b71292e7a020416012aa9f772b531581dccbb5
jiuqiwang <leonardowang597@gmail.com>
2022-02-26 11:32:51 -0700

    updated experimentation

--- experimentation.py
+++ experimentation.py
@@ -55,53 +55,103 @@ def test_runtime(start_rand_num = 52, end_rand_num = 62, interval = 1, num_trial
     with open('run_time.txt','wb') as file_out:
         pickle.dump(record_by_trial,file_out)
 
-def test_MCTS():
+def test_MCTS(base_sim_num = 20, start_sim_num = 10, end_sim_num = 100, interval = 10, game_num = 20, seed = 0):
+    # set random seed for reproducibility
+    np.random.seed(seed)
+    # uniform random prior policy
     pol = RandomPolicy()
+    # default(uniform random) simulation policy
     sim = RandomPlayer()
-    rand_player = RandomPlayer()
-    mcts_player = MCTSPlayer(pol,sim,50)
-    mcts2_player = MCTSPlayer(pol,sim,20) 
-    wins = 0
-    draws = 0
-    loses = 0
-    for i in tqdm(range(10)):
-        game = UltimateTTT(mcts_player,mcts2_player)
-        game.play()
-        if game.winner == 1:
-            wins += 1
-        elif game.winner == -1:
-            loses += 1
-        else:
-            draws += 1
-        mcts_player.reset()
-        mcts2_player.reset()
+    # baseline player
+    base_player = MCTSPlayer(pol,sim,base_sim_num)
 
-    
-    print('wins',wins)
-    print('loses',loses)
-    print('draws',draws)
+    win_rate = []
+    lose_rate = []
+    draw_rate = []
+
+    for test_sim_num in tqdm(range(start_sim_num, end_sim_num+1, interval)):
+
+        test_player = MCTSPlayer(pol,sim,test_sim_num)
+        num_wins = 0
+        num_draws = 0
+        num_loses = 0
+
+        for game in range(game_num):
+            # test player plays first
+            if game % 2 == 0:
+                game = UltimateTTT(test_player,base_player)
+                game.play()
+                if game.winner == 1:
+                    num_wins += 1
+                elif game.winner == -1:
+                    num_loses += 1
+                else:
+                    num_draws += 1
+            # base player plays first
+            else: 
+                game = UltimateTTT(base_player,test_player)
+                game.play()
+                if game.winner == 1:
+                    num_loses += 1
+                elif game.winner == -1:
+                    num_wins += 1
+                else:
+                    num_draws += 1
+
+            base_player.reset()
+            test_player.reset()
+        
+        win_rate.append((num_wins/game_num)*100)
+        lose_rate.append((num_loses/game_num)*100)
+        draw_rate.append((num_draws/game_num)*100)
+
+    record = [win_rate, lose_rate, draw_rate]
+    with open('test_mcts.txt','wb') as out_file:
+        pickle.dump(record,out_file)
 
 
-def plot_run_time(start_rand_num = 52, end_rand_num = 62, interval = 1, num_trials = 10):
+def plot_run_time(start_rand_num = 52, end_rand_num = 62, interval = 1):
     # read the data
     with open('run_time.txt','rb') as in_file:
         record = pickle.load(in_file)
     names = ['Boolean Minimax', 'Negamax', 'Alpha-Beta', 'Alpha-Beta TT', 'Proof Number Search']
     record = np.array(record)
     mean = np.mean(record,axis = 0)
+    plt.figure()
     for i in range(5):
         plt.plot(range(start_rand_num, end_rand_num+1, interval),np.log(mean[:,i]),label=names[i])
     
     plt.legend()
     plt.grid()
-    plt.title('Solver Running Times on log Scale against Number of Initial Random Plays')
+    plt.title('Solver Running Time Average on log Scale against Number of Initial Random Plays')
     plt.xlabel('Number of Initial Random Plays')
     plt.ylabel('Natural log of Running Time')
     plt.show()
 
-
+def plot_test_mcts(start_sim_num = 10, end_sim_num = 100, interval = 10, game_num=20):
+    with open('test_mcts.txt','rb') as in_file:
+        record = pickle.load(in_file)
+    
+    record = np.array(record)
+    plt.plot(range(start_sim_num,end_sim_num+1,interval),record[0,:],label='Win')
+    plt.plot(range(start_sim_num,end_sim_num+1,interval),record[1,:],label='Lose')
+    plt.plot(range(start_sim_num,end_sim_num+1,interval),record[2,:],label='Draw')
+    plt.title(f'Performance of the MCTS Player with Varying Degrees of Simulation againt the Baseline Player')
+    plt.xlabel('Number of Simulations')
+    plt.ylabel(f'Percentage on {game_num} Games')
+    plt.legend()
+    plt.grid()
+    plt.show()
+    
 def main():
-    test_MCTS()
+    player = RandomPlayer()
+    sample_game = UltimateTTT(player,player)
+    random_play(sample_game)
+    sample_game.display_board()
+    sample_game.display_board('outer')
+    plot_run_time()
+    plot_test_mcts()
+    
 
 if __name__ == '__main__':
     main()
