a8a81accfeb040a25e8c923a6c2c61b7810785d7
Jiuqi Wang <leonardowang597@gmail.com>
2022-10-13 21:50:11 -0600

    add checkpointing

--- alphazero/train.py
+++ alphazero/train.py
@@ -1,16 +1,13 @@
 import os
-import pickle
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from typing import List
 
-import haiku as hk
-import jax
-import jax.numpy as jnp
+import dill
 import optax
 import yaml
 from env.macros import *
 from env.ultimate_ttt import UltimateTTT
-from jax import grad, jit
+from jax import grad, jit, random
 from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm
 
@@ -22,7 +19,7 @@ from alphazero.replay_buffer import ReplayBuffer
 dir_path = os.path.dirname(__file__)
 
 
-def self_play(model_params: dict, model_state: dict, PRNGkey, sim_num: int, explore_factor: float, temperature: float, alpha:float, epsilon:float):
+def self_play(model_params: dict, model_state: dict, PRNGkey, sim_num: int, explore_factor: float, temperature: float, alpha: float, epsilon: float):
     px = AlphaZero(model_params, model_state, PRNGkey,
                    sim_num, explore_factor, temperature, alpha, epsilon)
     po = AlphaZero(model_params, model_state, PRNGkey,
@@ -62,15 +59,27 @@ def self_play(model_params: dict, model_state: dict, PRNGkey, sim_num: int, expl
     return px_trajectory, po_trajectory
 
 
-def train(total_games: int, games_per_train: int, iterations: int, lr: float, batch_size, seed: int, self_play_args: dict):
+def train(total_games: int, games_per_train: int, iters_per_train: int, ckpt_frequency: int, lr: float, batch_size, seed: int, self_play_args: dict, last_ckpt: int = 0):
+    assert last_ckpt >= 0
     model = create_model(True)
-    params, model_state = init_model(model)
     opt = optax.adamw(lr)
-    opt_state = opt.init(params)
-    rand_gen = hk.PRNGSequence(seed)
-    replay_buffer = ReplayBuffer(seed)
+    train_steps = last_ckpt
     writer = SummaryWriter(os.path.join(dir_path, 'log'))
-    train_steps = 0
+
+    if last_ckpt > 0:
+        ckpt_path = os.path.join(dir_path, f'checkpoints/{train_steps}.pickle')
+        with open(ckpt_path, 'rb') as fp:
+            ckpt = dill.load(fp)
+        params = ckpt['params']
+        model_state = ckpt['model_state']
+        opt_state = ckpt['opt_state']
+        replay_buffer = ckpt['replay_buffer']
+        rand_key = ckpt['rand_key']
+    else:
+        params, model_state = init_model(model)
+        opt_state = opt.init(params)
+        replay_buffer = ReplayBuffer(seed)
+        rand_key = random.PRNGKey(seed)
 
     @jit
     def loss_func(params, state, feature, true_score, search_prob):
@@ -90,12 +99,11 @@ def train(total_games: int, games_per_train: int, iterations: int, lr: float, ba
         next_params = optax.apply_updates(params, update)
         return next_params, next_model_state, next_opt_state, val_loss, pol_loss
 
-
     for index in tqdm(range(0, total_games, games_per_train)):
         with ThreadPoolExecutor(max_workers=4) as executor:
             res = []
             for _ in range(games_per_train):
-                next_key = rand_gen.next()
+                rand_key, next_key = random.split(rand_key)
                 res.append(executor.submit(self_play, model_params=params,
                            model_state=model_state, PRNGkey=next_key, **self_play_args))
 
@@ -104,8 +112,7 @@ def train(total_games: int, games_per_train: int, iterations: int, lr: float, ba
                 replay_buffer.add_traj(px_traj)
                 replay_buffer.add_traj(po_traj)
 
-
-        for _ in range(iterations):
+        for _ in range(iters_per_train):
             feature, search_prob, true_score = replay_buffer.sample_data(
                 batch_size)
             next_params, next_model_state, next_opt_state, val_loss, pol_loss = step(
@@ -117,13 +124,22 @@ def train(total_games: int, games_per_train: int, iterations: int, lr: float, ba
             train_steps += 1
             writer.add_scalar('value loss', val_loss.item(), train_steps)
             writer.add_scalar('policy loss', pol_loss.item(), train_steps)
-            
+
+        if (index + 1) % ckpt_frequency == 0:
+            checkpoint = {'params': params, 'model_state': model_state,
+                          'opt_state': opt_state, 'replay_buffer': replay_buffer, 'rand_key': rand_key}
+
+            ckpt_path = os.path.join(
+                dir_path, f'checkpoints/{train_steps}.pickle')
+
+            with open(ckpt_path, 'wb') as fp:
+                dill.dump(checkpoint, fp)
 
 
 def main():
     with open(os.path.join(dir_path, 'config.yaml'), 'r') as fp:
         config = yaml.safe_load(fp)
-    train(**config)
+    train(**config, last_ckpt=70)
 
 
 if __name__ == '__main__':
